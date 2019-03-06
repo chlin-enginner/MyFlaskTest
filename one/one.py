e2d0b6d6c918277b9d00df344906a9c4a62b603e
rudy <xian_sc@163.com>
2019-03-06 23:12:52 +0800

    新增

--- one/one.py
+++ one/one.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+import hashlib
 
 app = Flask(__name__)
 
@@ -6,6 +8,29 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+
+@app.route('/weixin', methods=['GET', 'POST'])
+def weixin():
+    if request.method == 'GET':
+        signature=request.args.get("signature")
+        timestamp = request.args.get("timestamp")
+        nonce = request.args.get("nonce")
+        echostr = request.args.get("echostr")
+
+        token = "csxwxapp"
+
+        list = [token, timestamp, nonce]
+        list.sort()
+        ts = ''.join(list)
+
+        hashcode = hashlib.sha1(ts.encode('utf-8')).hexdigest()
+
+        if hashcode == signature:
+            return echostr
+
+
 
 
 if __name__ == '__main__':
