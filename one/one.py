a8765ad93322910db7c9df8504a57e2b6c979d60
rudy <xian_sc@163.com>
2019-03-07 17:26:03 +0800

    新增

--- one/one.py
+++ one/one.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 from flask import request
 from flask import Response
 import hashlib
 import time
+import json
+import requests
 
 from lxml import etree
 import datetime
@@ -69,8 +73,18 @@ def weixin():
         run_days=(now-start_data).days
 
         if str(content)=="/help":
-            content="您好！欢迎您！本号旨在探讨个人数据分析场景，并尝试使用简单、友好的方式帮助个人实现数据分析的需求。已累计运行"+run_days+"天，实现AI聊天接入功能，后续会有更多分析功能上线，敬请期待！多谢关注！"
-            # res_hlep="<xml><ToUserName><![CDATA["+str(fromUser)+"]]></ToUserName><FromUserName><![CDATA["+str(touserName)+"]]></FromUserName><CreateTime>"+nowTime+"</CreateTime><MsgType><![CDATA["+msgType+"]]></MsgType><Content><![CDATA["+str(content_help)+"]]></Content></xml>"
+            content="您好！欢迎您！本号旨在探讨个人数据分析场景，并尝试使用简单、友好的方式帮助个人实现数据分析的需求。已累计运行"+str(run_days)+"天，实现AI聊天接入功能，后续会有更多分析功能上线，敬请期待！多谢关注！"
+
+        if not str(content).startswith("/"):
+
+            say_ai_data = {"reqType": 0, "perception": {"inputText": {"text": content}},
+                     "userInfo": {"apiKey": "3b27ac8f81ee4853be53a4d22211533a", "userId": "rudy"}}
+
+            api_url2 = "http://openapi.tuling123.com/openapi/api/v2"
+
+            req = requests.post(api_url2, json.dumps(say_ai_data)).json()
+            content=req['results'][0]['values']['text']
+
 
         res="<xml><ToUserName><![CDATA["+str(fromUser)+"]]></ToUserName><FromUserName><![CDATA["+str(touserName)+"]]></FromUserName><CreateTime>"+nowTime+"</CreateTime><MsgType><![CDATA["+msgType+"]]></MsgType><Content><![CDATA["+str(content)+"]]></Content></xml>"
 
